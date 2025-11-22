018bc9c58d27be8140059639bbb43bc56584582a
Aziz Arfaoui <aziz99arfaoui@gmail.com>
2025-11-22 01:28:23 +0100

    Add Creating and Combining Views tutorial project

---
+++ Creating and combining views/Creating and combining views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Creating and combining views
+//
+//  Created by Aziz Arfaoui on 22/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
